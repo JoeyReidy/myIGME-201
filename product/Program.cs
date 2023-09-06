22f009a5b9dd4db0bd51a3c0dc040ad786e17207
Joey Reidy <email4joeyreidy@gmail.com>
2023-09-06 14:33:26 -0400

    product

---
+++ product/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace product
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int int1 = 1;
+            int int2 = 1;
+            int int3 = 1;
+            int int4 = 1;
+            int result = 0;
+
+            Console.WriteLine("Please enter a number: ");
+            int1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number: ");
+            int2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a third number: ");
+            int3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a fourth number: ");
+            int4 = Convert.ToInt32(Console.ReadLine());
+
+            result = int1 * int2 * int3 * int4;
+
+            Console.WriteLine("The product of " + int1 + " * " + int2 + " * " + int3 + " * " + int4 + " is " + result);
+
+        }
+    }
+}
---
+++ varExpressions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varExpressions
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+namespace fabulous
+{
+    class fabClass
+    {
+        int great = 0;
+    }
+}
+
+namespace super
+{
+    namespace smashing
+    {
+        internal class ex
+        {
+            int newInt = varExpressions.great;
+        }
+
+    }
+}
+
+
