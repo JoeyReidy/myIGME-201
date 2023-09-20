f6e44376176e2836f4ef73abd2e54a054cb4f54e
Joey Reidy <email4joeyreidy@gmail.com>
2023-09-20 13:16:50 -0400

    quote me

---
+++ quote/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quote
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a string:");
+            string input = Console.ReadLine();
+            string output = "";
+            
+            string[] tokens = input.Split(' ');
+            foreach (string token in tokens)
+            {
+                output = output + "\"" + token + "\" ";
+            }
+            output = output + "\b";
+            Console.WriteLine(output);
+        }
+    }
+}
