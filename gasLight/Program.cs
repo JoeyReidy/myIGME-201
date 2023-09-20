1524245d55db85173a72db33ffa2b9298ac6659d
Joey Reidy <email4joeyreidy@gmail.com>
2023-09-20 13:13:16 -0400

    yes

---
+++ gasLight/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gasLight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a string:");
+
+            string input = Console.ReadLine();
+
+            string yes = input.Replace("no", "yes");
+            yes = input.Replace("No", "Yes");
+            yes = yes.Replace("NO", "YES");
+            yes = yes.Replace("no", "yes");
+            yes = yes.Replace("nO", "YeS");
+
+            Console.WriteLine(yes);
+            
+        }
+    }
+}
