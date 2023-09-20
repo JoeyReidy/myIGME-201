0ddb15d8c03929ef8c429dac9936d4424c90e300
Joey Reidy <email4joeyreidy@gmail.com>
2023-09-20 12:43:23 -0400

    reverse

---
+++ reverseString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a string:");
+            string input = Console.ReadLine();
+            string output = "";
+
+            for(int i = input.Length - 1; i >= 0; i--)
+            {
+                output = output + input[i];
+            }
+            
+            Console.WriteLine(output);
+        }
+    }
+}
