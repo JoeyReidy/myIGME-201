a93475dcef39389c00c2db9eaa2725510e651e50
Joey Reidy <email4joeyreidy@gmail.com>
2023-09-24 21:10:50 -0400

    delegate

---
+++ delegate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @delegate
+{
+    internal class Program
+    {
+        delegate string Input();
+        static void Main(string[] args)
+        {
+            Input input = Console.ReadLine;
+            Console.WriteLine("write something really cool: ");
+
+            string line = input();
+
+            Console.WriteLine(line);
+
+        }
+    }
+}
