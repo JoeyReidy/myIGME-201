616f2314a3020ecf65f34ddfd5d95aeb24f8cb00
Joey Reidy <email4joeyreidy@gmail.com>
2023-09-30 19:11:59 -0400

    delegate

---
+++ RoundDelegate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoundDelegate
+{
+    internal class Program
+    {
+        delegate double roundDelegate(double num, int roundBy);
+        static void Main(string[] args)
+        {
+            roundDelegate round = Math.Round;
+
+            double result = round(15.867, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
