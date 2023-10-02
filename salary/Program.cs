e6c5e7265b656f642db810aba221ad64019c00d0
Joey Reidy <email4joeyreidy@gmail.com>
2023-10-01 21:49:20 -0400

    salary

---
+++ salary/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salary
+{
+    internal class Program
+    {
+        static bool GiveRaise(string name, ref double salary)
+        {
+            if (name == "Joey")
+            {
+                Console.WriteLine("Congratulations! You got a raise!");
+                salary += 19999.99;
+                return true;
+                
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string sName;
+            double dSalary = 30000;
+
+            Console.WriteLine("Please enter your name: ");
+            sName = Console.ReadLine();
+            GiveRaise(sName, ref dSalary);
+            Console.WriteLine("Your salary is now $" + dSalary);
+        }
+    }
+}
