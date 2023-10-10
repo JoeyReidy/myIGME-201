f996d886c89dd82dc36cc5c14257ef8671ee1801
Joey Reidy <email4joeyreidy@gmail.com>
2023-10-10 15:27:17 -0400

    assPassenger

---
+++ Traffic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicles;
+
+namespace Traffic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SUV car = new SUV();
+            AddPassenger(car);
+        }
+
+        public static void AddPassenger(IPassengerCarrier vehicle)
+        {
+            vehicle.LoadPassenger();
+            Console.WriteLine(vehicle.ToString());
+        }
+    }
+}
