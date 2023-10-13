fb3ba498618c88ab7fe2cebd139590c694029620
Joey Reidy <email4joeyreidy@gmail.com>
2023-10-13 19:29:01 -0400

    Derived Class

---
+++ DerivedClass/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DerivedClass
+{
+    internal class Program
+    {
+        public class MyClass
+        {
+            private string myString;
+
+
+            public MyClass(string yourString)
+            {
+                myString = yourString;
+            }
+
+            public virtual string GetString()
+            {
+                return myString;
+            }
+
+            public string MyString { 
+                set { myString = value; }
+            }
+        }
+
+        public class MyDerivedClass : MyClass
+        {
+            public MyDerivedClass(string yourString) : base(yourString)
+            {
+
+            }
+            public override string GetString()
+            {
+                string overString = base.GetString() + " (output from the derived class)";
+                return overString;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+
