535eff788bafd8b3dc5bf7b0b13326e840f364d0
Joey Reidy <email4joeyreidy@gmail.com>
2023-10-19 21:10:21 -0400

    myMethod

---
+++ myMethod/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myMethod
+{
+    internal class Program
+    {
+        public interface myInterface
+        {
+            void theLine();
+        }
+
+        public class myClass : myInterface
+        {
+            public void theLine()
+            {
+                Console.WriteLine("this is the line");
+            }
+        }
+
+        public class myOtherClass : myInterface
+        {
+            public void theLine()
+            {
+                Console.WriteLine("this is the other line");
+            }
+        }
+
+        public static void MyMethod(object myObject)
+        {
+            if (myObject is myClass)
+            {
+                myClass myClass = (myClass)myObject;
+                myClass.theLine();
+            }
+            else if (myObject is myOtherClass)
+            {
+                myOtherClass myOtherClass = (myOtherClass)myObject;
+                myOtherClass.theLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            myClass myClass = new myClass();
+            myOtherClass myOtherClass = new myOtherClass();
+            MyMethod(myClass);
+            MyMethod(myOtherClass);
+        }
+    }
+}
