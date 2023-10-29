4ad39e0e7f78ea57cbf6f0510cf50e8a95c04af3
Joey Reidy <email4joeyreidy@gmail.com>
2023-10-29 16:07:03 -0400

    phones

---
+++ Phone/Program.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone
+{
+    internal class Program
+    {
+        public abstract class Phone
+        {
+            private string phoneNumber;
+            public string address;
+
+            public string PhoneNumber;
+            public abstract void Connect();
+            public abstract void Disconnect();
+        }
+
+        public interface PhoneInterface
+        {
+            void Answer();
+            void MakeCall();
+            void HangUp();
+        }
+
+        public class RotaryPhone : Phone, PhoneInterface
+        {
+            public void Answer()
+            {
+
+            }
+            public void MakeCall()
+            {
+
+            }
+            public void HangUp()
+            {
+
+            }
+
+            public override void Connect()
+            {
+
+            }
+            public override void Disconnect()
+            {
+
+            }
+        }
+
+        public class PushButtonPhone : Phone, PhoneInterface
+        {
+            public void Answer()
+            {
+
+            }
+            public void MakeCall()
+            {
+
+            }
+            public void HangUp()
+            {
+
+            }
+
+            public override void Connect()
+            {
+
+            }
+            public override void Disconnect()
+            {
+
+            }
+        }
+
+        public class Tardis : RotaryPhone
+        {
+            private bool sonicScrewdriver;
+            private byte whichDrWho;
+            private string femaleSideKick;
+            public double exteriorSurfaceArea;
+            public double interiorVolume;
+
+            public byte WhichDrWho { get; }
+            public string FemaleSideKick { get; }
+
+            public void TimeTravel()
+            {
+
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (obj is Tardis)
+                {
+                    Tardis other = (Tardis)obj;
+                    return (this.WhichDrWho == other.WhichDrWho);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            public static bool operator <(Tardis tardis, Tardis other)
+            {
+                if(tardis.WhichDrWho == 10 && other.WhichDrWho != 10)
+                {
+                    return tardis > other;
+                }
+                else if (tardis.WhichDrWho != 10 && other.WhichDrWho == 10)
+                {
+                    return tardis < other;
+                }
+                return tardis.WhichDrWho < other.WhichDrWho;
+            }
+            public static bool operator >(Tardis tardis, Tardis other)
+            {
+                if (tardis.WhichDrWho == 10 && other.WhichDrWho != 10)
+                {
+                    return tardis > other;
+                }
+                else if (tardis.WhichDrWho != 10 && other.WhichDrWho == 10)
+                {
+                    return tardis < other;
+                }
+                return tardis.WhichDrWho > other.WhichDrWho;
+            }
+            public static bool operator <=(Tardis tardis, Tardis other)
+            {
+                if (tardis.WhichDrWho == 10 && other.WhichDrWho != 10)
+                {
+                    return tardis > other;
+                }
+                else if (tardis.WhichDrWho != 10 && other.WhichDrWho == 10)
+                {
+                    return tardis < other;
+                }
+                return tardis.WhichDrWho <= other.WhichDrWho;
+            }
+            public static bool operator >=(Tardis tardis, Tardis other)
+            {
+                if (tardis.WhichDrWho == 10 && other.WhichDrWho != 10)
+                {
+                    return tardis > other;
+                }
+                else if (tardis.WhichDrWho != 10 && other.WhichDrWho == 10)
+                {
+                    return tardis < other;
+                }
+                return tardis.WhichDrWho >= other.WhichDrWho;
+            }
+            public static bool operator ==(Tardis tardis, Tardis other)
+            {
+                return tardis.WhichDrWho == other.WhichDrWho;
+            }
+            public static bool operator !=(Tardis tardis, Tardis other)
+            {
+                return tardis.WhichDrWho != other.WhichDrWho;
+            }
+        }
+
+        public class PhoneBooth : PushButtonPhone
+        {
+            private bool superMan;
+            public double costPerCall;
+            public bool phoneBook;
+
+            public void OpenDoor()
+            {
+
+            }
+            public void CloseDoor()
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Tardis tardis = new Tardis();
+            PhoneBooth booth = new PhoneBooth();
+        }
+
+        static void UsePhone(object obj)
+        {
+            if (obj is PhoneInterface)
+            {
+                PhoneInterface phone = (PhoneInterface)obj;
+
+                if(obj is Tardis)
+                {
+                    Tardis tardis = (Tardis)obj;
+                    tardis.TimeTravel();
+                }
+                else if (obj is PhoneBooth)
+                {
+                    PhoneBooth booth = (PhoneBooth)obj;
+                    booth.OpenDoor();
+                }
+
+                phone.MakeCall();
+                phone.HangUp();
+            }
+            else
+            {
+                Console.WriteLine("you cannot make a phone call without a phone.");
+            }
+        }
+    }
+}
---
+++ tupleMath/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tupleMath
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<(double, double, double), double> formula = new SortedList<(double, double, double), double>();
+            double z;
+            for (double w = -2; w <= 0; w = w + 0.2)
+            {
+                for(double y = -1; y<= 1; y = y + 0.1)
+                {
+                    for(double x = 0; x <= 4; x = x + 0.1)
+                    {
+                        z = (4 * Math.Pow(y, 3)) + (2 * Math.Pow(x, 2)) - 8 * w + 7;
+                        z = Math.Round(z, 3);
+                        formula.Add((w, x, y), z);
+                        
+                    }
+                }
+            }
+        }
+    }
+}
