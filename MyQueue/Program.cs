92928c09def53600353a56db538eac614fe4bc92
Joey Reidy <email4joeyreidy@gmail.com>
2023-12-18 15:25:47 -0500

    myqueue

---
+++ MyQueue/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQueue
+{
+    class MyQueue
+    {
+        private List<int> queue;
+        public MyQueue()
+        {
+            queue = new List<int>();
+        }
+
+
+        public void Enqueue(int n)
+        {
+            queue.Add(n);
+        }
+        public int Dequeue()
+        {
+            if (queue.Count <= 0)
+            {
+                throw new Exception("No integers to dequeue");
+            }
+            else
+            {
+                int last = queue[0];
+                queue.RemoveAt(0);
+                return last;
+            }
+        }
+
+        public int Peek()
+        {
+            if(queue.Count <= 0)
+            {
+                throw new Exception("queue is empty");
+            }
+            else { return queue[0]; }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
