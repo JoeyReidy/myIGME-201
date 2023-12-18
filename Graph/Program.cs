3d881450dca4ffd9217854378f3904e32bc6bac2
Joey Reidy <email4joeyreidy@gmail.com>
2023-12-18 16:28:08 -0500

    graph

---
+++ Graph/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    internal class Program
+    {
+        static bool[,] mGraph = new bool[,]
+        {
+           { false   , true    , true   , false   , false    , false   , false   , false },
+           { false   , false   , false   , true    , true   , false   , false   , false },
+           { false    , false   , false   , true   , false   , true   , false   , false },
+           { false   , true   , true   , false   , false   , false   , false   , false },
+           { false   , false   , false   , false   , false   , false   , false    , true },
+           { false   , false   , false   , false   , false   , false   , true   , false },
+           { false   , false   , false   , false   , true   , false   , false   , false },
+           { false   , false   , false   , false   , false   , false    , false   , false }
+        };
+
+        static int[][] lraph = new int[][]
+        {
+            new int[] { 2, 3 },
+            new int[] { 3, 4 },
+            new int[] { 3, 5 },
+            new int[] { 2, 1 },
+            new int[] { 7 },
+            new int[] { 6 },
+            new int[] { 4 },
+            null
+        };
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+}
